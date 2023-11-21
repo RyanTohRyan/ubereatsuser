bc7bf862f5d0d6dfaa0d8f38a21db2398b8c8c4c
RyanTohRyan <70338573+RyanTohRyan@users.noreply.github.com>
2023-11-22 00:20:09 +0800

    2:55

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import { StyleSheet, Text, View , FlatList} from 'react-native';
 
 import HomeScreen from './src/screens/HomeScreen';
 import RestaurantDetailsPage from './src/screens/RestaurantDetailsScreen';
+import DishDetailsScreen from './src/screens/DishDetailsScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-<RestaurantDetailsPage/>
+{/* <RestaurantDetailsPage/> */}
 {/* <HomeScreen />      */}
+<DishDetailsScreen/>
     </View>
   );
 }
